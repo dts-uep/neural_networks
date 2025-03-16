f28fde521e9629c90217aa4c26931cc977876d5f
dts-uep <trantrongdathppc@gmail.com>
2025-03-17 02:56:12 +0700

    Add drawing box

--- object_detection/WindowsSliding_HOG_SVM/src/main.py
+++ object_detection/WindowsSliding_HOG_SVM/src/main.py
@@ -1,5 +1,15 @@
 import numpy as np
 import math
+import cv2
+
+
+# Get data
+def image_data(folder_path):
+    pass
+
+# Grayscale
+def gray_scale(image:np.array):
+    return 0.2989*image[:, :, 2]+0.5870*image[:, :, 1]+0.1140*image[:, :, 0]
 
 
 # One hot encoder
@@ -87,37 +97,37 @@ def SobelFiltering(image_list:list, threshold:int=0.05, return_new_images:bool=F
     im_flt_list = []
     orientation_list = []
     
-    for image in image_list:
-        # X direction
-        filter_x = np.asarray([
+    filter_x = np.asarray([
             [-1, 0, 1],
             [-2, 0, 2],
             [-1, 0, 1]
             ])
-        
+    
+    filter_y = np.asarray([
+            [-1, -2, -1],
+            [ 0,  0,  0],
+            [ 1,  2,  1]
+        ])
+    
+    for image in image_list:
+        # X direction    
         Gx = np.zeros(image.shape)
         
         for i in range(image.shape[1] - 2):
             for j in range(image.shape[0] - 2):
                 filtered = filter_x * image[j:j+3, i:i+3]
                 filtered = filtered.sum()
-                Gx[j + 1, i + 1] = filtered / 510.0 # Scale down 510, Max = 1020, Min = -1020
+                Gx[j + 1, i + 1] = filtered / 64 # Scale down by cell size
         Gx_denominator = np.where(Gx==0, 0.00001, Gx)
         
-        # Y direction
-        filter_y = np.asarray([
-            [-1, -2, -1],
-            [ 0,  0,  0],
-            [ 1,  2,  1]
-        ])
-        
+        # Y direction   
         Gy = np.zeros(image.shape)
          
         for i in range(image.shape[1] - 2):
             for j in range(image.shape[0] - 2):
                 filtered = filter_y * image[j:j+3, i:i+3]
                 filtered = filtered.sum()
-                Gy[j + 1, i + 1] = filtered / 510.0
+                Gy[j + 1, i + 1] = filtered / 64 # Scale down by cell size
          
         # Calculate magnitude
         G = np.sqrt(Gx**2 + Gy**2)
@@ -176,7 +186,7 @@ class SVM():
     
     def __init__(self, n_class:int, input_size:int):
         
-        self.__W = np.random.rand(n_class, input_size+1) / input_size
+        self.__W = np.random.rand(n_class, input_size+1) / (input_size + 1)*10 # Get initial of around 0 to 1
         self.__X = None
         self.__Y = None
         self.__Z = None
@@ -187,14 +197,14 @@ class SVM():
     
     
     def __forward__(self):
-        self.__Z = np.dot(self.__W, self.__X)
+        self.__Z = np.dot(self.__W, self.__X) 
     
     
     def __update_weight__(self):
         
         # Calculate loss
         Ztrue = np.where(self.__Y==1, self.__Z, 0).sum(axis=0)
-        Loss_matrix = 1 - Ztrue + self.__Z
+        Loss_matrix = 0.5 - Ztrue + self.__Z # Choose 0.5 to get result close to 1
         Loss_matrix = np.maximum(Loss_matrix, 0)
         Loss_matrix = np.where(self.__Y == 1, 0, Loss_matrix)
         self.__loss = Loss_matrix.sum()
@@ -212,7 +222,7 @@ class SVM():
         del gradient_count
         
         # Update parameters
-        self.__W -= self.__lr*gradient_matrix / self.__X.shape[1]
+        self.__W -= self.__lr*gradient_matrix / (self.__X.shape[1]+1)**2 # Scale down by batch size and origin down_scale ratio
         
     
     def fit(self, X:list, Y:list, epochs:int=1, lr:float=0.001):
@@ -230,6 +240,8 @@ class SVM():
             self.__update_weight__()
             
             print(f"Loss: {self.__loss}")
+            if self.__loss <= 0.00001:
+                break
      
 
     def predict(self, X:list):
@@ -274,12 +286,11 @@ def object_detect(image:np.array, window_size_list:list, rotation_list:list, cla
     # Windows rotation of alpha to check object rotation of negative alpha
     rotation_list = [math.radians(alpha) for alpha in rotation_list]
     
-    result = []
+    results = []
     
     for w in window_size_list:
         for f in flip_list:
             for a in rotation_list:
-                attributes = [w, f, a]
                 sliding_list = []
                 location_list = []
                 
@@ -331,7 +342,7 @@ def object_detect(image:np.array, window_size_list:list, rotation_list:list, cla
                         del f_slide
                         
                         sliding_list.append(r_slide)
-                        location_list.append((r*w/3, c*w/3))
+                        location_list.append((int(r*w/3), int(c*w/3)))
                 del r_slide
                 
                 # resize
@@ -339,18 +350,51 @@ def object_detect(image:np.array, window_size_list:list, rotation_list:list, cla
                 sliding_list = image_resize(sliding_list, (resize_size, resize_size))
                 
                 # HOG + SVM
-                class_prob_predict = classifier.predict(HOG(sliding_list))
+                list = HOG(sliding_list)
+                class_prob_predict = classifier.predict(list)
                 
                 # Remove none categorized window and append in to result
-                result.append([(attributes,
+                results += [(w,
                                location_list[i],
-                               class_prob_predict[:, i]) for i in range(class_prob_predict.shape[1])
-                               if np.all(class_prob_predict[:, i]) > threshold])
+                               class_prob_predict[:, i] > threshold) for i in range(class_prob_predict.shape[1])
+                               if np.any(class_prob_predict[:, i] > threshold)]
                 
-    return result     
+    return results     
                      
-                
 
+# Draw box
+def display_detected_image(image:np.array, results:list, class_list:list, box_size_ratio:list):
+    
+    for result in results:
+        box_width = result[0]
+        position_x = result[1][1]
+        position_y = result[1][0]
+        labels = np.argwhere(result[2])
+        
+        for object in labels[0]:
+            # Shape box for different class
+            wth = int(box_width * box_size_ratio[object][0])
+            lgth = int(box_width * box_size_ratio[object][1])
+            
+            x = int(position_x + (box_width - wth) / 2)
+            y = int(position_y + (box_width - lgth) / 2)
+            
+            # Draw box
+            image[y:y+2, x:x+wth, :] = np.array([0, 0, 255]) # up margin
+            image[y+lgth-2:y+lgth, x:x+wth, :] = np.array([0, 0, 255]) # down margin
+            image[y:y+lgth, x:x+2, :] = np.array([0, 0, 255]) # left margin
+            image[y:y+lgth, x+wth-2:x+wth, :] = np.array([0, 0, 255]) # right margin
+            
+            # Draw label
+            cv2.putText(image, class_list[object], (x, y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 1, cv2.LINE_AA)
+    
+    image = np.uint8(image)
+    cv2.imshow("Detected image" ,image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+        
 # TEST
 def main():
     
@@ -367,27 +411,27 @@ def main():
     print(encoder.reverse_transform(encoder.fit_transform(Y)))
     
     svm = SVM(encoder.vocab_size, 225)
-    svm.fit(HOG(image_list), encoder.transform(Y), epochs=500, lr=0.01)
+    svm.fit(HOG(image_list), encoder.transform(Y), epochs=10000, lr=0.001)
     
     image_list_test = [np.random.randint(0, 255, (40, 40))]
     print(svm.predict(HOG(image_list=image_list_test)))
     print(svm.predict_label(HOG(image_list=image_list_test)))
     print(encoder.reverse_transform(svm.predict_label(HOG(image_list=image_list_test))))
     print(encoder.vocab)
-    
+    print(svm.predict(HOG(image_list=image_list[0:1])))
     print(encoder.reverse_transform(svm.predict_label(HOG(image_list=image_list[0:1]))))
     
-    fake_image = np.random.randint(0, 255, (400, 700))
+    fake_image = np.random.randint(0, 255, (400, 700, 3))
+    fake_image_gray = gray_scale(fake_image)
     # Window size should be divisible by 3
-    print(object_detect(fake_image, window_size_list=[81, 162, 201, 300], rotation_list=[0, 45, 90, 180], classifier=svm, threshold=0.1, flip=False))
+    results = object_detect(fake_image_gray, window_size_list=[162, 201, 300], rotation_list=[0, 90, 180], classifier=svm, threshold=0.7, flip=False)
+    
+    display_detected_image(fake_image, results=results, class_list=encoder.vocab, box_size_ratio=[(1, 1), (1, 1), (1/3, 2/3), (1/3, 2/3)])
+    
 
 main()
 
 
 
-# Test
-def test():
-    pass
 
-test()
     
